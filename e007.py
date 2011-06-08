7bbe7749eda8d12130ca28142a53f099a1084b7f
Alvaro <zephyr.3@gmail.com>
2011-06-08 08:59:19 -0700

    Added 7th euler problem

---
+++ e007.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+"""
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10001st prime number?
+"""
+import math, sys
+def isPrime(number):
+	if number == 2 or number == 3:
+		return True
+	numMax = int(math.ceil(math.sqrt(number)))
+	i = 2
+	bPrime = True
+	while i <= numMax :
+		if number % i == 0 :
+			return False
+		i += 1
+	return bPrime
+
+#target = 6
+target = 10001
+primeCount = 0
+i = 2
+while primeCount <= target :
+	if isPrime(i):
+		primeCount += 1
+		if primeCount == target:
+			break;
+	if i < 3:
+		i += 1
+	else :
+		i += 2
+print "============="
+print i
